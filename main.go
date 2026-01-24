6edd75abd5d491b104cb7748b1faf92ae12d034e
Jason Sochin <jsochin8@gmail.com>
2026-01-24 10:08:13 -0800

    fix: moved handler logic out of main into seperate functions

--- main.go
+++ main.go
@@ -8,92 +8,107 @@ import (
 	"github.com/nihcosnosaj/jason-blog/internal/service"
 )
 
+const (
+	templateGlob  = "templates/*"
+	defaultPort   = "8080"
+	layoutTmpl    = "layout"
+	aboutSlug     = "about"
+	bookshelfSlug = "bookshelf"
+)
+
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+	router.Static("assets/", "./assets")
 
 	// force redirect HTTP to HTTPS
-	/*
-		Note: this only works if the reverse proxy sets this header.
-		If running locally or behind a proxy that doesn't set it,
-		this will break.
-	*/
 	router.Use(redirectToHTTPS())
 
-	// Mount static assets
-	router.Static("/assets", "./assets")
+	router.GET("/", homeHandler)
+	router.GET("/blog", blogListHandler)
+	router.GET("/about", aboutHandler)
+	router.GET("/bookshelf", bookshelfHandler)
+	router.GET("/post/:slug", postHandler)
 
-	// Landing Page
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "layout", gin.H{
-			"IsHome": true,
-		})
-	})
-
-	// Blog List Page
-	router.GET("/blog", func(c *gin.Context) {
-		posts, err := service.GetAllPosts()
-		if err != nil {
-			c.HTML(http.StatusInternalServerError, "layout", gin.H{"Title": "Internal Server Error"})
-			return
-		}
-
-		fmt.Printf("Found %d posts\n", len(posts))
-
-		c.HTML(http.StatusOK, "layout", gin.H{
-			"Title":  "Blog",
-			"Posts":  posts,
-			"IsBlog": true,
-		})
-	})
-
-	// About Page
-	router.GET("/about", func(c *gin.Context) {
-		post, err := service.GetPostBySlug("about")
-		if err != nil {
-			c.HTML(http.StatusNotFound, "layout", gin.H{"Title": "Not Found"})
-			return
-		}
-		c.HTML(http.StatusOK, "layout", gin.H{
-			"Title":   post.Title,
-			"Content": post.Content,
-			"IsPost":  true,
-		})
-	})
-
-	// Bookshelf Page
-	router.GET("/bookshelf", func(c *gin.Context) {
-		post, err := service.GetPostBySlug("bookshelf")
-		if err != nil {
-			c.HTML(http.StatusNotFound, "layout", gin.H{"Title": "Not Found"})
-			return
-		}
-		c.HTML(http.StatusOK, "layout", gin.H{
-			"Title":       post.Title,
-			"Content":     post.Content,
-			"IsBookshelf": true,
-		})
-	})
-
-	// Single Post Page
-	router.GET("/post/:slug", func(c *gin.Context) {
-		slug := c.Param("slug")
-		post, err := service.GetPostBySlug(slug)
-		if err != nil {
-			c.HTML(http.StatusNotFound, "layout", gin.H{"Title": "Not Found"})
-			return
-		}
-		c.HTML(http.StatusOK, "layout", gin.H{
-			"Title":   post.Title,
-			"Date":    post.Date,
-			"Content": post.Content,
-			"IsPost":  true,
-		})
-	})
-
-	router.Run(":8080")
+	router.Run(":" + defaultPort)
 }
 
+// Handler definitions.
+
+func homeHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, layoutTmpl, gin.H{
+		"IsHome": true,
+	})
+}
+
+func blogListHandler(c *gin.Context) {
+	posts, err := service.GetAllPosts()
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, layoutTmpl, gin.H{
+			"Title": "Internal Server Error",
+		})
+		return
+	}
+
+	fmt.Printf("Found %d posts\n", len(posts))
+
+	c.HTML(http.StatusOK, layoutTmpl, gin.H{
+		"Title":  "Blog",
+		"Posts":  posts,
+		"IsBlog": true,
+	})
+
+}
+
+func aboutHandler(c *gin.Context) {
+	post, err := service.GetPostBySlug(aboutSlug)
+	if err != nil {
+		c.HTML(http.StatusNotFound, layoutTmpl, gin.H{
+			"Title": "Not Found",
+		})
+		return
+	}
+	c.HTML(http.StatusOK, layoutTmpl, gin.H{
+		"Title":   post.Title,
+		"Content": post.Content,
+		"IsPost":  true,
+	})
+}
+
+func bookshelfHandler(c *gin.Context) {
+	post, err := service.GetPostBySlug(bookshelfSlug)
+	if err != nil {
+		c.HTML(http.StatusNotFound, layoutTmpl, gin.H{
+			"Title": "Not Found",
+		})
+		return
+	}
+	c.HTML(http.StatusOK, layoutTmpl, gin.H{
+		"Title":       post.Title,
+		"Content":     post.Content,
+		"IsBookshelf": true,
+	})
+}
+
+func postHandler(c *gin.Context) {
+	slug := c.Param("slug")
+	post, err := service.GetPostBySlug(slug)
+	if err != nil {
+		c.HTML(http.StatusNotFound, layoutTmpl, gin.H{
+			"Title": "Not Found",
+		})
+		return
+	}
+	c.HTML(http.StatusOK, layoutTmpl, gin.H{
+		"Title":   post.Title,
+		"Date":    post.Date,
+		"Content": post.Content,
+		"IsPost":  true,
+	})
+}
+
+// Gin middleware that redirects HTTP requests to HTTPS
+// if the X-Forwarded-Proto header is set to "http"
 func redirectToHTTPS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if c.Request.Header.Get("X-Forwarded-Proto") == "http" {
